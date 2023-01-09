856b63189d4dce486046ade2fd24eec17c10b7bf
Austin Papritz <austin@papritz.dev>
2023-01-09 13:04:43 -0800

    animals.js done but getting error

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,5 +5,18 @@ import { animals } from '../../src/data.js';
 import Animal from '../Animal/Animal.js';
 
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${background})` }}></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal
+          key={animal.name}
+          name={animal.name}
+          type={animal.type}
+          says={animal.says}
+          top={animal.top}
+          left={animal.left}
+        />
+      ))}
+    </main>
+  );
 }
