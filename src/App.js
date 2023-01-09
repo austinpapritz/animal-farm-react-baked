68c0982fb8da15f7e88ccac017eb99f4094a9594
Austin Papritz <austin@papritz.dev>
2023-01-09 15:39:37 -0800

    removed backgroundImage from app.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
-import background from '../src/background.png';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main backgroundImage={background} />
+      <Main />
       <Footer />
     </div>
   );
